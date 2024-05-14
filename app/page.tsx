dccfffe135890e30c1b4ad86a1d2499099b764c3
Satpal Singh Rathore <satpal.code@gmail.com>
2024-05-14 08:23:49 +0530

    font

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import Head from 'next/head'
 import Hero from "@/components/Hero"
 import HeroLight from "@/components/HeroLight"
 export default function Home() {
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>Dataformer App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet"/>
+      </Head>
+      <div>
       {/* <Hero/> */}
       <HeroLight/>
     </div>
+    </>
   )
 }
