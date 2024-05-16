07ea4e3d4723c4bdfae38415ca1b8ec25a322309
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-05-16 19:49:31 +0530

    mobile responsive,vertical allign, img, buttons donee

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,6 @@ import { TwitterTweetEmbed } from 'react-twitter-embed';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-interface DatabaseIconProps extends SVGProps<SVGSVGElement> {}
 
 
 function Hero() {
@@ -27,11 +26,11 @@ const particlesLoaded = useCallback(async (container: Container | undefined) => 
 const settings = {
   dots: true,
   infinite: true,
-  speed: 2000,
+  speed: 4000,
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 0,
+  autoplaySpeed: 500,
   responsive: [
     {
       breakpoint: 1024,
@@ -67,28 +66,28 @@ return (
         <div className="mx-auto mt-2 h-1 w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"></div>
     </div>
     
-        <p className="max-w-[800px] text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed mt-4">
+        <p className="max-w-[800px] mt-10 text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
         Create, curate and clean datasets for your custom Large Language Model fine-tuning.
         </p>
 
-        <div className="mt-8">
+        <div className="mt-10">
           <Link target="_blank" href={"https://github.com/BhabhaAI"} >
-        <button className="   relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-xs xl:text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button className=" relative inline-flex items-center justify-center p-0.5 mb-2 mr-3 me-2 overflow-hidden text-sm xl:text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Explore Github
 </span>
 </button>
           </Link>
           <Link target="_blank" href={"https://cal.com/satpal/30min"} >
-<button className="relative     inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-xs xl:text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+<button className="relative     inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm xl:text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
 <span className="relative  px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Book a call
 </span>
 </button>
           </Link>
         </div>
-        <div className="mt-4">
-        <a href="/about" className="hover:underline text-gray-200 ">
+        <div className="mt-7">
+        <a href="/about" className=" text-lg hover:underline text-gray-200 ">
             Learn more  
         </a>
         </div>
@@ -136,15 +135,15 @@ Book a call
       <Image className="w-[80%] rounded-xl" src={image_section} alt="" />
     </div>
     {/* image section ends here */}
-
-
     {/* below is the testimonial section */}
 
     <div className="flex justify-center mt-20">
         <h1 className="text-4xl text-white">Testimonials</h1>
     </div>
       <div className="rounded-b-2xl ml-8 mt-10 w-[90%]">
-        <Slider {...settings}>
+      
+{/* Render slider once tweets are loaded */}
+  <Slider {...settings}>
           <div className="px-1">
             <div className="tweet-container">
               <TwitterTweetEmbed tweetId="1785410133646061727" />
@@ -192,7 +191,7 @@ Book a call
           </div>
         </Slider>
       </div>
-            </>
+    </>
 );
 }
 
